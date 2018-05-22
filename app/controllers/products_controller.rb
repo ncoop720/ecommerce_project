537ebc1b179e29ebec61617b0ed59fd9b03873d2
Nathan Cooper <ncoop720@yahoo.com>
2018-05-22 08:11:12 -0400

    devise, navbar, rating limits, rating stars, styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
+  
   # GET /products
   # GET /products.json
   def index
@@ -42,6 +42,7 @@ class ProductsController < ApplicationController
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
+    @categories = Category.all
     respond_to do |format|
       if @product.update(product_params)
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,7 @@ class Product < ApplicationRecord
 	belongs_to :category
 
 	validates :image, presence: :true
+
+	validates :rating, format: /[1-5]/
+
 end
