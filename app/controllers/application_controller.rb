0a48d34513f0f2da89a6fd093fdef9a4678df978
Nathan Cooper <ncoop720@yahoo.com>
2018-05-24 07:53:48 -0400

    finishing touches for application and cart controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :current_order
+
 	def configure_permitted_parameters
 	   devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
 	   devise_parameter_sanitizer.permit(:account_update, keys: [:role])
@@ -23,5 +25,13 @@ class ApplicationController < ActionController::Base
 	def brands
 		@brands = Product.pluck(:brand).sort.uniq
 	end
+
+	def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
 
 end
--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,36 +2,73 @@ class CartController < ApplicationController
 	before_action :authenticate_user!, except: [:add_to_cart, :view_order]
 
   def add_to_cart
-    if LineItem.find_by(product_id: params[:product_id])
-      line_item = LineItem.find_by(product_id: params[:product_id])
-      line_item.update(quantity: (line_item.quantity + params[:quantity].to_i))
+   #  if LineItem.find_by(product_id: params[:product_id])
+   #    line_item = LineItem.find_by(product_id: params[:product_id])
+   #    line_item.update(quantity: (line_item.quantity + params[:quantity].to_i))
+   #  else
+   #    line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity].to_i)
+   #  end
+
+   #  line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
+	  # redirect_back(fallback_location: root_path)
+
+    #create an order earlier using our helper method
+    @order = current_order
+    # get all the items from our line_item table
+    line_item = @order.line_items.find_by(product_id: params[:product_id])
+  
+    #check to see if product already exists in our cart to prevent doubles
+    if !line_item.blank?
+       line_item.update(quantity: line_item.quantity += params[:quantity].to_i)
     else
-      line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity].to_i)
+      #if the item is already in the cart, update the quantity  
+      line_item = @order.line_items.new(product_id: params[:product_id], quantity: params[:quantity])
+      
+      @order.save
+      session[:order_id] = @order.id
     end
 
     line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
-	  redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   def view_order
-  	@line_items = LineItem.all
+    @line_items = current_order.line_items
   end
 
   def checkout
-  	line_items = LineItem.all
-  	@order = Order.create(user_id: current_user.id, subtotal: 0)
+  	# line_items = LineItem.all
+  	# @order = Order.create(user_id: current_user.id, subtotal: 0)
 
-  	line_items.each do |line_item|
-  		line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
-  		@order.order_items[line_item.product.id] = line_item.quantity
-  		@order.subtotal += line_item.line_item_total
-  	end
-  	@order.save
+  	# line_items.each do |line_item|
+  	# 	line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
+  	# 	@order.order_items[line_item.product.id] = line_item.quantity
+  	# 	@order.subtotal += line_item.line_item_total
+  	# end
+  	# @order.save
 
-  	@order.update(sales_tax: (@order.subtotal * 0.08))
-  	@order.update(grand_total: (@order.sales_tax + @order.subtotal))
+  	# @order.update(sales_tax: (@order.subtotal * 0.08))
+  	# @order.update(grand_total: (@order.sales_tax + @order.subtotal))
 
-  	line_items.destroy_all
+  	# line_items.destroy_all
+
+    line_items = current_order.line_items
+
+    unless line_items.length == 0
+      current_order.update(user_id: current_user.id, subtotal: 0)
+      @order = current_order
+
+      line_items.each do |line_item|
+        line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
+        @order.order_items[line_item.product_id] = line_item.quantity 
+        @order.subtotal += line_item.line_item_total
+      end
+
+      @order.save
+      @order.update(sales_tax: (@order.subtotal * 0.08))
+      @order.update(grand_total: (@order.sales_tax + @order.subtotal))
+      session[:order_id] = nil
+    end
   end
 
   def edit_line_item
@@ -50,6 +87,7 @@ class CartController < ApplicationController
 
   def order_complete
     @order = Order.find(params[:order_id])
+    @order.line_items.destroy_all
     @amount = (@order.grand_total.to_f.round(2) * 100).to_i
 
     customer = Stripe::Customer.create(
